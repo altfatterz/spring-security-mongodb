9b776d511ac68534a94f71fd4d9c94b2087c0950
Zoltan Altfatter <altfatterz@gmail.com>
2015-02-09 22:15:49 +0100

    add custom mongodb authentication provider, login is works for every user

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,7 +1,11 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +20,22 @@ public class Application {
     @RequestMapping("/")
     public String home() {
        return "Hello World";
+    }
+
+    @Configuration
+    protected static class SecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+        @Autowired
+        private MongoDBAuthenticationProvider authenticationProvider;
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.authenticationProvider(authenticationProvider);
+        }
+
+        // Check this: http://stackoverflow.com/questions/25633477/security-configuration-with-spring-boot
+
+
     }
 
 }
