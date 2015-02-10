ab1dbe5bbc5758c1307b89f4b40e8746fe3c68af
Zoltan Altfatter <altfatterz@gmail.com>
2015-02-10 14:23:43 +0100

    add test using embedded MongoDB

--- src/test/java/demo/AuthenticationTests.java
+++ src/test/java/demo/AuthenticationTests.java
@@ -1,16 +1,18 @@
 package demo;
 
+import org.apache.commons.codec.binary.Base64;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,5 +32,25 @@ public class AuthenticationTests {
         ResponseEntity<String> response = template.getForEntity("http://localhost:" + port + "/", String.class);
         assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
     }
+
+    @Test
+    public void login() {
+        ResponseEntity<String> response = template.exchange("http://localhost:" + port + "/",
+                HttpMethod.GET, new HttpEntity<String>(createHeaders("altfatterz@gmail.com", "secret")), String.class);
+
+        assertEquals("Hello World", response.getBody());
+    }
+
+    private HttpHeaders createHeaders(final String username, final String password ){
+        return new HttpHeaders(){
+            {
+                String auth = username + ":" + password;
+                byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("UTF-8")));
+                String authHeader = "Basic " + new String( encodedAuth );
+                set( "Authorization", authHeader );
+            }
+        };
+    }
+
 
 }
